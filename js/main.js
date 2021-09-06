3c3798773e24685cca5d6892da16963d655efc45
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-09-06 22:13:06 +0100

    Add listener to close class

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const hamburger = document.querySelector('#navToggle');
-// let div =document.querySelector('div')
-
-// hamburger.addEventListener("click" , () => {
-//     div.classList.add('Hamburger-me')
-// }
-// )
+let close = document.querySelector('#cancel');
+let img = document.querySelector('#cancel');
+close.addEventListener("click" , () => {
+img.classList.remove('Hamburger-me');
+}
+)
 
 let div = document.querySelector('#navToggle');
 hamburger.addEventListener("click" , () => {
