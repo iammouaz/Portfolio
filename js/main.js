499afb884546ac12bce323d70494add831a16269
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-07 00:02:18 +0300

    add js file
    add the JavaScript Listener and connect with html doc

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('#navToggle');
+// let div =document.querySelector('div')
+
+// hamburger.addEventListener("click" , () => {
+//     div.classList.add('Hamburger-me')
+// }
+// )
+
+let div = document.querySelector('#navToggle');
+hamburger.addEventListener("click" , () => {
+div.classList.add('Hamburger-me');
+}
+)
