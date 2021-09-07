5382d049dbe4f424fe23176dd03c29ac1a22d92a
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-07 03:08:45 +0300

    modify the style and some html codes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const close = document.querySelector('#cancel');
 
 let div = document.querySelector('#navToggle');
 hamburger.addEventListener("click" , () => {
-div.classList.toggle('Hamburger-me');
+div.classList.toggle('Hamburger-active');
 }
 )
 
