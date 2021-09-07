a1accf7f106b7fb97e79ef0c56d5ebe1ae50c198
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-08 00:31:13 +0300

    add function to add html popup

--- js/main.js
+++ js/main.js
@@ -22,3 +22,26 @@ const contactmeclose = document.querySelector('#contact_me-link');
 contactmeclose.addEventListener('click', () => {
   div.classList.remove('Hamburger-active');
 });
+
+const projects = [
+  {
+    headerCard: 'Profesional Art Printing Data More',
+    textCard: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    badgesCard: ['html', 'bootstrap', 'Ruby'],
+    imageURL: './assets/Snapshoot-Portfolio.png',
+    liveURL: 'https://www.github.com',
+    projectURL: 'https://www.github.com/iammouaz',
+  },
+];
+const popup = document.getElementById('popup');
+projects.forEach((item) => {
+  popup.innerHTML += `
+<h2 class="popup-name">
+${item.headerCard}
+</h2>
+<div class="snapshoot"></div>
+<p class="p-popup">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+</p>
+
+`;
+});
