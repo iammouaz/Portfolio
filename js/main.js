48daee7a9ad87134b0ddcf4b8db4a5f90691c3f5
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-08 00:52:16 +0300

    update on js

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ projects.forEach((item) => {
 ${item.headerCard}
 </h2>
 <div class="snapshoot"></div>
-<p class="p-popup">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+<p class="p-popup">${item.textCard}
 </p>
 
 `;
