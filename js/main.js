594e1b24554a09463ff90664535dcd5e2785bfa4
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-07 13:32:25 +0300

    fixing some issues on mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-const hamburger = document.querySelector('#navToggle');
+const hamburger = document.querySelector('#hamburger-img');
 const div = document.querySelector('#navToggle');
 hamburger.addEventListener('click', () => {
   div.classList.toggle('Hamburger-active');
+});
+
+const imgburger = document.querySelector('#cancel');
+imgburger.addEventListener('click', () => {
+  div.classList.remove('Hamburger-active');
 });
